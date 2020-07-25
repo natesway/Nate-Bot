9dab2a8ee725e0f1a518e72c8a4f72ea64f522ed
natesway <64743334+natesway@users.noreply.github.com>
2020-07-25 18:04:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 
 bot.on("ready", function (){
      console.log('S[bot.user.username] Is Online!');
+     bot.user.setActivity("hello", {type: "WATCHING", url:"https://youtu.be/KqpQoIZl3Og"})
 });
 
 bot.login(process.env.token);
