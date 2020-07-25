5d11d0303daefe1af35bdecc3b3fc6e62642b4bc
natesway <64743334+natesway@users.noreply.github.com>
2020-07-25 18:08:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 bot.on("ready", function (){
      console.log('S[bot.user.username] Is Online!');
-     bot.user.setActivity("https://youtu.be/KqpQoIZl3Og", {type: "WATCHING", url:"https://youtu.be/KqpQoIZl3Og"})
+     bot.user.setActivity("John Cena Play Gacha Life While Sitting On The Toilet And Listening To His Theme Song And Farting Alot", {type: "WATCHING", url:"https://youtu.be/KqpQoIZl3Og"})
 });
 
 bot.login(process.env.token);
